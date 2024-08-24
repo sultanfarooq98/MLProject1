f1d272c96cf53b428b0540da6631fac0bca740a7
Sultan Farooq <sultanfarooq98@gmai.com>
2024-08-24 16:04:55 +0500

    EDA ad Problem Statement

--- src/exception.py
+++ src/exception.py
@@ -1,16 +1,14 @@
 import sys
+from src.logger import logging
 
-def error_message_details(error,error_detail:sys):
-  _,_, exc_tb=error_detail.exc_info()
+def error_message_details(error, error_detail:sys):
+    _, _, exc_tb = error_detail.exc_info()
   
-  file_name=exc_tb.tb_frame.f_code.co_filename
-  error_message="Error occured in python script name [{0}] line number [{1}] error message[{2}]".format(
+    file_name = exc_tb.tb_frame.f_code.co_filename
+    error_message = "Error occurred in python script name [{0}] line number [{1}] error message[{2}]".format(
+        file_name, exc_tb.tb_lineno, str(error))
 
-    file_name,exc_tb.tb_lineno, str(error)
-
-return = error_message
-
-  )
+    return error_message
 
 class CustomException(Exception):
     def __init__(self,error_message, error_detail:sys):
@@ -20,4 +18,11 @@ class CustomException(Exception):
     def __str__(self):
        return self.error_message
     
-    
+# if __name__=="__main__":
+#     logging.info("Logging has started")
+
+#     try:
+#        a=1/0
+#     except Exception as e:
+#        logging.info("Divide by Zero")
+#        raise CustomException(e,sys)
